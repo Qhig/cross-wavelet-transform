f0083cdb5a26727d57115966d7dd903d9544d56c
Qhig <79509757+Qhig@users.noreply.github.com>
2022-02-17 09:42:40 +0100

    Update plotting_example.py

--- plotting_example.py
+++ plotting_example.py
@@ -11,7 +11,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Import the cross-wavelet transform function from the cwt.py file
-from cwt import cfs
+from xwt import xwt
 
 ## Loading datas
 
@@ -22,7 +22,7 @@ fs = np.load('fs.npy')
 
 
 ## Usage of the cfs function
-WXamp, WXspec, WXangle, Wcoh, WXdt, freqs, coi = cfs(ori_waveform, new_waveform, fs, 3, 0.25, 10, 0.5, 7, 100)
+WXamp, WXspec, WXangle, Wcoh, WXdt, freqs = xwt(ori_waveform, new_waveform, fs, 3, 0.25, 10, 0.5, 7, 100)
 
 
 ## Plotting
@@ -34,7 +34,6 @@ plt.tight_layout()
 plt.pcolormesh(time, freqs, WXdt, cmap='jet_r', edgecolors='none')
 plt.clim([-0.02, 0.01])
 plt.colorbar()
-plt.plot(time, coi, 'w--', linewidth=5)
 plt.ylim(freqs[-1], freqs[0])
 plt.xlim(4, 25)
 plt.title('Smoothed Time difference', fontsize=13)
@@ -46,7 +45,6 @@ plt.tight_layout()
 plt.pcolormesh(time, freqs, Wcoh, cmap='jet', edgecolors='none')
 plt.clim([0.985, 1])
 plt.colorbar()
-plt.plot(time, coi, 'w--', linewidth=5)
 plt.ylim(freqs[-1], freqs[0])
 plt.xlim(4, 25)
 plt.title('Wavelet Coherence', fontsize=13)
@@ -58,7 +56,6 @@ plt.tight_layout()
 plt.pcolormesh(time, freqs, np.log(WXamp), cmap='jet', edgecolors='none')
 plt.clim([-50, 0])
 plt.colorbar()
-plt.plot(time, coi, 'w--', linewidth=5)
 plt.ylim(freqs[-1], freqs[0])
 plt.xlim(4, 25)
 plt.title('(Logarithmic) Amplitude', fontsize=13)
